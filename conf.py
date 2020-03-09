f3211090cef9fafc5efabf5d8b049fdeb1671623
error <59992348+MotoiSakura@users.noreply.github.com>
2020-03-09 20:50:07 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -21,12 +21,12 @@ enable_jsdelivr = {
 # 站点设置
 site_name = "我的个人博客"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+site_build_date = "2020-02-20T10:21+08:00"
+author = "神秘人"
+email = "sample@foxmail.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+description = "冬天來了，春天還會遠嗎？"
+key_words = ['Maverick', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
